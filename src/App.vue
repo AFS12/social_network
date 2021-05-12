ba8734e24068d786805517c4c52a79436688ad9a
AFS12 <arthurmbayres@gmail.com>
2021-05-12 19:57:36 -0300

    - Top bar first version

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,10 @@
   <v-app>
     <v-app-bar
       app
-      color="primary"
+      color="grey darken-4"
       dark
     >
-      <div class="d-flex align-center">
+      <container class="d-flex align-center">
         <v-img
           alt="Vuetify Logo"
           class="shrink mr-2"
@@ -14,26 +14,43 @@
           transition="scale-transition"
           width="40"
         />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
+        <v-container>
+          <h1>Social_Network</h1>
+        </v-container>
+      </container>
 
       <v-spacer></v-spacer>
-
+      <v-tabs centered icons-and-text>
+          <v-tab><v-icon>mdi-home</v-icon></v-tab>
+          <v-tab><v-icon>mdi-monitor-dashboard</v-icon></v-tab>
+          <v-tab><v-icon>mdi-store</v-icon></v-tab>
+          <v-tab><v-icon>mdi-account-group</v-icon></v-tab>
+          <v-tab><v-icon>mdi-gamepad</v-icon></v-tab>
+        </v-tabs>
+      <v-spacer></v-spacer>
       <v-btn
         href="https://github.com/vuetifyjs/vuetify/releases/latest"
         target="_blank"
+        rounded
         text
       >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
+        <v-img src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png" width="30"></v-img>
+        <span class="mr-2">Arthur</span>
+      </v-btn>
+      <v-btn fab small rounded color="grey darken-3">
+         <v-icon>mdi-dots-grid</v-icon>
+      </v-btn>
+      -
+      <v-btn fab small rounded color="grey darken-3">
+        <v-icon>mdi-facebook-messenger</v-icon>
+      </v-btn>
+      -
+      <v-btn fab small rounded color="grey darken-3">
+        <v-icon>mdi-bell</v-icon>
+      </v-btn>
+      -
+      <v-btn fab small rounded color="grey darken-3">
+        <v-icon>mdi-menu-down</v-icon>
       </v-btn>
     </v-app-bar>
 
@@ -58,3 +75,9 @@ export default {
   }),
 };
 </script>
+
+<style>
+  #Search{
+    padding-top: 10px;
+  }
+</style>
