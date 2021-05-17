7fd347a64a34edbd254f9dd95a75df7129fdc480
AFS12 <arthurmbayres@gmail.com>
2021-05-17 14:08:11 -0300

    - Two Side drawer align

--- src/App.vue
+++ src/App.vue
@@ -1,81 +1,93 @@
 <template>
   <v-app>
     <v-container fluid>
-      <v-app-bar
-      app
-      color="grey darken-4"
-      dark
-      fixed
-    >
-      <container class="d-flex align-center">
-        
-        <v-container>
-          
-        </v-container>
-      </container>
+      <v-app-bar app color="grey darken-4" dark fixed>
 
-      <v-spacer></v-spacer>
-      <v-tabs centered icons-and-text>
+        <v-tabs centered icons-and-text>
           <v-tab><v-icon>mdi-home</v-icon></v-tab>
           <v-tab><v-icon>mdi-monitor-dashboard</v-icon></v-tab>
           <v-tab><v-icon>mdi-store</v-icon></v-tab>
           <v-tab><v-icon>mdi-account-group</v-icon></v-tab>
           <v-tab><v-icon>mdi-gamepad</v-icon></v-tab>
         </v-tabs>
-      <v-spacer></v-spacer>
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        rounded
-        text
-      >
-        <v-img src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png" width="30"></v-img>
-        <span class="mr-2">Arthur</span>
-      </v-btn>
-      <v-btn fab small rounded color="grey darken-3">
-         <v-icon>mdi-dots-grid</v-icon>
-      </v-btn>
-      -
-      <v-btn fab small rounded color="grey darken-3">
-        <v-icon>mdi-facebook-messenger</v-icon>
-      </v-btn>
-      -
-      <v-btn fab small rounded color="grey darken-3">
-        <v-icon>mdi-bell</v-icon>
-      </v-btn>
-      -
-      <v-btn fab small rounded color="grey darken-3">
-        <v-icon>mdi-menu-down</v-icon>
-      </v-btn>
-    </v-app-bar>
+        <v-spacer></v-spacer>
+      </v-app-bar>
     </v-container>
-    <v-navigation-drawer  fixed app v-model='drawer' :mini-variant.sync="mini">
-      <v-list-item>
+    <v-navigation-drawer fixed app v-model="drawer" :mini-variant.sync="mini" left>
+      <v-list-item class="grey darken-4" dark>
         <v-list-item-content color="primary">
           <v-list-item-title class="title">
-            <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="30"
-        />
+            <v-btn
+              rounded
+              outlined
+              height="40"
+              text
+            >
+              <v-img
+                src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+                width="25"
+              ></v-img>
+              <span class="mr-2 ">Arthur Ayres</span>
+            </v-btn>
           </v-list-item-title>
         </v-list-item-content>
       </v-list-item>
 
       <v-divider></v-divider>
 
-      <v-list
-        dense
-        nav
-      >
-        <v-list-item
-          v-for="item in items"
-          :key="item.title"
-          link
-        >
+      <v-list dense nav>
+        <v-list-item v-for="item in items" :key="item.title" link>
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-navigation-drawer
+      fixed
+      app
+      v-model="drawer"
+      :mini-variant.sync="mini"
+      right
+    >
+      <v-list-item class="grey darken-4">
+        <v-list-item-content color="primary">
+          <v-list-item-title class="title">
+            <v-row>
+              <v-col cols="12" md="3">
+                <v-btn fab small rounded elevation="0">
+                  <v-icon>mdi-dots-grid</v-icon>
+                </v-btn>
+              </v-col>
+              <v-col cols="12" md="3">
+                <v-btn fab small rounded elevation="0">
+                  <v-icon>mdi-facebook-messenger</v-icon>
+                </v-btn>
+              </v-col>
+              <v-col cols="12" md="3">
+                <v-btn fab small rounded elevation="0">
+                  <v-icon>mdi-bell</v-icon>
+                </v-btn>
+              </v-col>
+              <v-col cols="12" md="3">
+                <v-btn fab small rounded elevation="0">
+                  <v-icon>mdi-menu-down</v-icon>
+                </v-btn>
+              </v-col>
+            </v-row>
+          </v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list dense nav>
+        <v-list-item v-for="item in items" :key="item.title" link>
           <v-list-item-icon>
             <v-icon>{{ item.icon }}</v-icon>
           </v-list-item-icon>
@@ -87,41 +99,38 @@
       </v-list>
     </v-navigation-drawer>
     <v-main>
-      <v-container fluid>
-       
+      <v-container fluid> </v-container>
 
-      </v-container>
-      
-      <HelloWorld/>
+      <HelloWorld />
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import HelloWorld from "./components/HelloWorld";
 
 export default {
-  name: 'App',
+  name: "App",
 
   components: {
-    HelloWorld,
+    HelloWorld
   },
 
-  data () {
-    return{
+  data() {
+    return {
       items: [
-          { title: 'Dashboard', icon: 'mdi-view-dashboard' },
-          { title: 'Photos', icon: 'mdi-image' },
-          { title: 'About', icon: 'mdi-help-box' },
-        ],
-        right: null,
-    }
-  },
+        { title: "Dashboard", icon: "mdi-view-dashboard" },
+        { title: "Photos", icon: "mdi-image" },
+        { title: "About", icon: "mdi-help-box" }
+      ],
+      right: null
+    };
+  }
 };
 </script>
 
 <style>
-  #Search{
-    padding-top: 10px;
-  }
+#Search {
+  padding-top: 10px;
+}
 </style>
