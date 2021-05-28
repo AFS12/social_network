3f0602f6321052aace249e454857a14a9c840b80
AFS12 <arthurmbayres@gmail.com>
2021-05-28 20:24:27 -0300

    - Upgrading pub Cards

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <template>
   <v-container>
-    <v-row align="center" justify="center" v-for="index in 5" :key="index">
+    <v-row align="center" justify="center" v-for="index in posts" :key="index">
       <v-col cols="12" sm="8">
         <v-card>
           <v-card-title>
@@ -9,13 +9,13 @@
               max-height="50"
               max-width="50"
             ></v-img>
-            <span class="mr-2 ">Arthur Ayres</span>
+            <span class="mr-2 ">{{index.name}}</span>
           </v-card-title>
           <v-card-text>
             <h3>ALGUMA PUB</h3>
             <br>
             <v-img
-              src="https://abglt.org.br/wp-content/uploads/2020/10/wallpaper-pc1-scaled.jpg"
+              :src= index.img
               max-height="1000"
             ></v-img>
           </v-card-text>
@@ -32,7 +32,27 @@ export default {
     return {
       drawer: true,
       mini: true,
-      numbers: [ 1, 2, 3, 4, 5 ]
+      posts: [ 
+        {
+          img: 'https://abglt.org.br/wp-content/uploads/2020/10/wallpaper-pc1-scaled.jpg',
+          name: 'Arthur Ayres',
+        }, 
+        {
+          img: 'https://i.pinimg.com/originals/0a/4d/cb/0a4dcb92fa2d3c601b58d72720d6bec4.jpg',
+          name: 'Julia Goncalves',
+        }, 
+        {
+          img: 'https://steamuserimages-a.akamaihd.net/ugc/940586530515504757/CDDE77CB810474E1C07B945E40AE4713141AFD76/',
+          name: 'Juia Do Carmo',
+        }, 
+        {
+          img: 'https://wallpapercave.com/wp/wp7864479.png',
+          name: 'Paçoca Mendonça',
+        }, 
+        {
+          img: 'https://psxbrasil.com.br/wp-content/uploads/2020/09/TLOU-2-4K.jpg',
+          name: 'GameShop',
+        } ]
     };
     
   }
